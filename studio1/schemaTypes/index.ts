04e2e8c5b92a182953b6ec55bf1a4d112ef15be0
Al Barret <mespelid@gmail.com>
2025-10-03 23:20:09 +0200

    fjernet studio1 og studio2

--- studio1/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-// studio1/schemaTypes/index.ts
-import chapter from './models/chapter'
-import page from './models/page'
-import siteSettings from './models/siteSettings'
-import imageWithCaption from './objects/imageWithCaption'
-
-export const schemaTypes = [siteSettings, page, chapter, imageWithCaption]
-
