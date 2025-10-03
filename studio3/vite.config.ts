66fe563f8632f6cedf7378c2a5f5b53f9308d0c8
Al Barret <mespelid@gmail.com>
2025-10-04 00:14:10 +0200

    vite

--- studio3/vite.config.ts
+++ studio3/vite.config.ts
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      '*.repl.co',
+      '*.repl.run'
+    ]
+  },
   preview: {
-    allowedHosts: ["*.repl.co", "*.repl.run"]
+    allowedHosts: [
+      '*.repl.co',
+      '*.repl.run'
+    ]
   }
 });
