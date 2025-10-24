3568bf57d7857f966d146803a30fdcf5dff93db1
Al Barret <mespelid@gmail.com>
2025-10-24 18:45:48 +0200

    group lang posts

--- studio3/sanity.config.ts
+++ studio3/sanity.config.ts
@@ -1,9 +1,10 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {documentInternationalization} from '@sanity/document-internationalization'
-import {schemaTypes} from './schemaTypes'
-import {translateAction} from './plugins/translateAction'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { documentInternationalization } from '@sanity/document-internationalization'
+import { schemaTypes } from './schemaTypes'
+import { translateAction } from './plugins/translateAction'
+import { postsStructure } from './structure/postsStructure'
 
 export default defineConfig({
   name: 'default',
@@ -13,36 +14,40 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [
-    structureTool(),
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Content')
+          .items([
+            postsStructure(S),
+            S.divider(),
+            S.documentTypeListItem('author').title('Authors'),
+            S.documentTypeListItem('category').title('Categories'),
+          ]),
+    }),
     visionTool(),
     documentInternationalization({
       supportedLanguages: [
-        {id: 'en', title: 'English'},
-        {id: 'de', title: 'Deutsch'},
-        {id: 'es', title: 'Español'},
-        {id: 'fr', title: 'Français'},
-        {id: 'no', title: 'Norsk'},
+        { id: 'en', title: 'English' },
+        { id: 'de', title: 'Deutsch' },
+        { id: 'es', title: 'Español' },
+        { id: 'fr', title: 'Français' },
+        { id: 'no', title: 'Norsk' },
       ],
       schemaTypes: ['post', 'category'],
     }),
   ],
 
-  schema: {
-    types: schemaTypes,
-  },
+  schema: { types: schemaTypes },
 
   document: {
     actions: (prev, context) => {
       if (context.schemaType === 'post') {
-        // inject client from context
         return [translateAction(context.getClient), ...prev]
       }
       return prev
     },
   },
 
-  server: {
-    port: 5000,
-    hostname: '0.0.0.0',
-  },
+  server: { port: 5000, hostname: '0.0.0.0' },
 })
