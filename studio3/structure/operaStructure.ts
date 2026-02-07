fe97dfd08f1a2e1400bf03b5729bed806fb0b11b
Al Barret <mespelid@gmail.com>
2026-02-07 02:00:31 +0100

    backka

--- studio3/structure/operaStructure.ts
+++ studio3/structure/operaStructure.ts
@@ -1,74 +1,19 @@
+// /studio3/structure/operaStructure.ts
 import {StructureBuilder} from 'sanity/structure'
-import {DocumentsIcon, UsersIcon, BookIcon, MusicNoteIcon} from '@sanity/icons'
 
 export const operaStructure = (S: StructureBuilder) =>
   S.listItem()
-    .title('Opera Database')
-    .icon(DocumentsIcon)
+    .title('Operas')
     .child(
       S.list()
-        .title('Content')
+        .title('Opera content')
         .items([
-          // 1. All Operas List
-          S.documentTypeListItem('opera').title('All Operas'),
+          S.documentTypeListItem('opera').title('Operas'),
 
           S.divider(),
 
-          // 2. Musical Numbers (Grouped by Opera)
-          S.listItem()
-            .title('Musical Numbers')
-            .icon(MusicNoteIcon)
-            .child(
-              S.documentTypeList('opera')
-                .title('Select Opera')
-                .child((operaId) =>
-                  S.documentList()
-                    .title('Musical Numbers')
-                    .schemaType('musicalNumber')
-                    .filter('_type == "musicalNumber" && opera._ref == $operaId')
-                    .params({operaId})
-                    .initialValueTemplates([
-                      S.initialValueTemplateItem('musicalNumber-by-opera', {operaId})
-                    ])
-                )
-            ),
-
-          // 3. Characters (Grouped by Opera)
-          S.listItem()
-            .title('Characters')
-            .icon(UsersIcon)
-            .child(
-              S.documentTypeList('opera')
-                .title('Select Opera')
-                .child((operaId) =>
-                  S.documentList()
-                    .title('Characters')
-                    .schemaType('operaCharacter')
-                    .filter('_type == "operaCharacter" && opera._ref == $operaId')
-                    .params({operaId})
-                    .initialValueTemplates([
-                      S.initialValueTemplateItem('operaCharacter-by-opera', {operaId})
-                    ])
-                )
-            ),
-
-          // 4. Libretti (Grouped by Opera)
-          S.listItem()
-            .title('Libretti')
-            .icon(BookIcon)
-            .child(
-              S.documentTypeList('opera')
-                .title('Select Opera')
-                .child((operaId) =>
-                  S.documentList()
-                    .title('Libretti')
-                    .schemaType('librettoText')
-                    .filter('_type == "librettoText" && opera._ref == $operaId')
-                    .params({operaId})
-                    .initialValueTemplates([
-                      S.initialValueTemplateItem('librettoText-by-opera', {operaId})
-                    ])
-                )
-            ),
+          S.documentTypeListItem('musicalNumber').title('Musical numbers'),
+          S.documentTypeListItem('operaCharacter').title('Characters'),
+          S.documentTypeListItem('librettoText').title('Libretti'),
         ])
     )
